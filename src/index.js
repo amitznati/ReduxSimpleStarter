61c412b819f596edfc5696953a36f09330601766
amitznati-work <amitznati@gmail.com>
2018-03-18 15:43:34 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyC_GzE5UvLbPXBJN6QKwNL4hRiAdqAOkbY';
+
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+ReactDOM.render(<App />,document.querySelector('.container'));
